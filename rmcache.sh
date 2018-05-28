b6d1c12573374bf576e36de5f6bef218a8a09465
Joel Aro <joelaro.to@gmail.com>
2018-05-28 16:58:34 -0400

    Fixed rm not deleting files/folders. Script now can be passed a JSON file for testing

--- rmcache.sh
+++ rmcache.sh
@@ -16,15 +16,13 @@ if [[ ${1: -5} == ".json" ]]; then FILE=$1; fi
 
 # Take the arguments passed to the script and convert them into something that
 # the jq function can use.
-SEARCH_ARGS_STRING=$(echo ${@:2} | tr ' ' '\n' | awk 'NR==1 { printf ".name==\"%s\"", $0 } NR>1 { printf " or .name==\"%s\"", $0 }')
-QUERY=$(jq ".apps[] | select($SEARCH_ARGS_STRING)" $FILE)
-echo $QUERY | while read -r $(jq -rc .paths[]); do echo $path; done
+SEARCH_ARGS_STRING=$(echo ${@:2} | tr ' ' '\n' \
+	| awk 'NR==1 { printf ".name==\"%s\"", $0 } NR>1 { printf " or .name==\"%s\"", $0 }')
+
 # For each of the apps listed in the json file, delete all the files at the
 # specified paths
-
-
-
-# Delete paths in each object
-#echo $QUERY
-#echo $QUERY | while read -r $(jq -r .paths[]); do echo $path; done
-#echo "$QUERY" | jq -r ".paths[]" | while read "path"; do rm -rf $path; done
+jq ".apps[] | select($SEARCH_ARGS_STRING)" $FILE | jq ".paths[]"\
+	| while read path
+	do
+		echo $path | tr ' ' '\ ' | xargs rm -rf
+	done
