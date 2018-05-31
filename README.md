7f91c10a47b0a9c0c75abd57c13cbb7968ee6606
Joel Aro <joelaro.to@gmail.com>
2018-05-31 14:51:13 -0400

    Added a README.md to the repo.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hello world!
+rmcache is a script that makes clearing the cache on any OSX app (or groups of them) easier!
+
+## To-do
+[ ] Add CI tools
--- test/test.sh
+++ test/test.sh
@@ -7,4 +7,4 @@ mkdir -p "~/Library/Application Support/testfolder3"
 touch "/Library/Application Support/testfolder1/file1"
 touch "~/Library/Application Support/testfolder3/file2"
 
-./rmcache.sh test.json TestApp
+../rmcache.sh ../json/test.json TestApp
